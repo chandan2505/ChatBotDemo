fe66932d58646c051e8fced2adc54299001ee318
chandan2505 <chandan2505@gmail.com>
2018-02-25 23:28:54 +1100

    changed logs to string

--- src/main/java/com/demo/chatbot/ChatBotDemo/ChatBotRestController.java
+++ src/main/java/com/demo/chatbot/ChatBotDemo/ChatBotRestController.java
@@ -13,12 +13,12 @@ public class ChatBotRestController {
 	@RequestMapping(value = "/bot/calculatePrem", method = RequestMethod.POST)
 	public Fulfillment calculatePremium(AIOriginalRequest request) {
 
-		System.out.println("Request::::" + request);
+		System.out.println("Request::::" + request.toString());
 		Fulfillment fulfillment = new Fulfillment();
 
 		fulfillment.setDisplayText("welcome from java");
 		fulfillment.setSpeech("Your Loan is 2000 AUD");
-		System.out.println("Response::::" + fulfillment);
+		System.out.println("Response::::" + fulfillment.toString());
 		return fulfillment;
 	}
 }
