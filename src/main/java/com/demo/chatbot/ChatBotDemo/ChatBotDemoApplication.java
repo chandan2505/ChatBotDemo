6cc8075ce0b37cb7f835856571bd21ad0a356962
chandan2505 <chandan2505@gmail.com>
2018-02-25 21:37:48 +1100

    Initial commit

---
+++ src/main/java/com/demo/chatbot/ChatBotDemo/ChatBotDemoApplication.java
@@ -0,0 +1,12 @@
+package com.demo.chatbot.ChatBotDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBotDemoApplication.class, args);
+	}
+}
