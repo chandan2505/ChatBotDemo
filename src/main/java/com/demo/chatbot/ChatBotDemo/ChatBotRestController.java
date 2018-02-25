5f4501e020ac093f766fbab0def5c5ea7eb0e2ea
chandan2505 <chandan2505@gmail.com>
2018-02-25 23:25:15 +1100

    added logs

--- src/main/java/com/demo/chatbot/ChatBotDemo/ChatBotRestController.java
+++ src/main/java/com/demo/chatbot/ChatBotDemo/ChatBotRestController.java
@@ -4,22 +4,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import ai.api.model.AIRequest;
+import ai.api.model.AIOriginalRequest;
 import ai.api.model.Fulfillment;
 
 @RestController
 public class ChatBotRestController {
 
 	@RequestMapping(value = "/bot/calculatePrem", method = RequestMethod.POST)
-	public Fulfillment calculatePremium(AIRequest request) {
-		 System.out.println("In Test one Method");
-		 System.out.println( "request::::"+request+"::::String"+request.toString());
-				 System.out.println( "getEntities::::"+request.getEntities().size());
-		    Fulfillment fulfillment = new Fulfillment();
-		    
-		    fulfillment.setDisplayText("welcome from java");
-		    fulfillment.setSpeech("Your Loan is 2000 AUD");
+	public Fulfillment calculatePremium(AIOriginalRequest request) {
 
-		    return fulfillment;
+		System.out.println("Request::::" + request);
+		Fulfillment fulfillment = new Fulfillment();
+
+		fulfillment.setDisplayText("welcome from java");
+		fulfillment.setSpeech("Your Loan is 2000 AUD");
+		System.out.println("Response::::" + fulfillment);
+		return fulfillment;
 	}
 }
