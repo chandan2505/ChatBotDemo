beded82c7597a7548e1ad632d3a27f7550fa1ee2
chandan2505 <chandan2505@gmail.com>
2018-02-26 00:26:23 +1100

    Parsing request JSON

--- src/main/java/com/demo/chatbot/ChatBotDemo/ChatBotRestController.java
+++ src/main/java/com/demo/chatbot/ChatBotDemo/ChatBotRestController.java
@@ -1,9 +1,14 @@
 package com.demo.chatbot.ChatBotDemo;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import ai.api.model.Fulfillment;
 
@@ -15,9 +20,22 @@ public class ChatBotRestController {
 
 		System.out.println("Request:::" + request);
 		Fulfillment fulfillment = new Fulfillment();
-
 		fulfillment.setDisplayText("welcome from java");
-		fulfillment.setSpeech("Your Loan is 2000 AUD");
+
+		Gson gson = new Gson();
+		Map<String, Object> map = gson.fromJson(request, new TypeToken<Map<String, Object>>() {
+		}.getType());
+		@SuppressWarnings("unchecked")
+		String prprtyVal = (String) ((Map<String, Object>) ((Map<String, Object>) map.get("result")).get("parameters"))
+				.get("propertyValue");
+		System.out.println(prprtyVal);
+		if (prprtyVal.equalsIgnoreCase("5000000")) {
+			fulfillment.setSpeech("Your Loan is 2000 AUD");
+		} else if (prprtyVal.equalsIgnoreCase("7000000")) {
+			fulfillment.setSpeech("Your Loan is 3000 AUD");
+		} else if (prprtyVal.equalsIgnoreCase("9000000")) {
+			fulfillment.setSpeech("Your Loan is 4000 AUD");
+		}
 		System.out.println("Response::::" + fulfillment.toString());
 		return fulfillment;
 	}
