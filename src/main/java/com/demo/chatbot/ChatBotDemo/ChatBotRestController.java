7fe5c46fcd1899136376aedbc3496f9f91e481af
chandan2505 <chandan2505@gmail.com>
2018-02-25 22:09:29 +1100

    test chatbot

--- src/main/java/com/demo/chatbot/ChatBotDemo/ChatBotRestController.java
+++ src/main/java/com/demo/chatbot/ChatBotDemo/ChatBotRestController.java
@@ -1,19 +1,22 @@
 package com.demo.chatbot.ChatBotDemo;
 
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import ai.api.model.AIRequest;
+import ai.api.model.Fulfillment;
 
 @RestController
 public class ChatBotRestController {
 
 	@RequestMapping(value = "/bot/calculatePrem", method = RequestMethod.POST)
-	public String calculatePremium(@RequestBody String loan) {
-		if (loan.equalsIgnoreCase("500000")) {
-			return "Your Loan is 3000 AUD";
-		}
+	public Fulfillment calculatePremium(AIRequest loan) {
+		 System.out.println("In Test one Method");
+		    Fulfillment fulfillment = new Fulfillment();
+		    fulfillment.setDisplayText("welcome from java");
+		    fulfillment.setSpeech("Your Loan is 2000 AUD");
 
-		return "Your Loan is 2000 AUD";
+		    return fulfillment;
 	}
 }
